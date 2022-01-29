54a4c668c8f4f2be655f4724259e17082eca2aac
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-01-29 07:42:47 -0500

    created a working form with setting state and handle functions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,6 @@ import Nav from "../Nav";
 const Header = () => {
   return (
     <div>
-      header
       <Nav />
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>NaV</div>;
+  const [formValue, setFromValue] = useState("");
+
+  const handlechange = (e) => {
+    setFromValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="Search-bar">Search:</label>
+        <input onChange={handechange} type="text" value={formValue} />
+        <button type="submit">Click</button>
+      </form>
+    </div>
+  );
 };
 
 export default Nav;
