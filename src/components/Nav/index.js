e3180267ccd3a81731b7534c3f2714e681fba683
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-01-29 08:53:03 -0500

    installed axios, testing fetching api data

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 const Nav = () => {
   const [formValue, setFromValue] = useState("");
+  const [videoId, setVideoId] = useState([]);
 
   const handleChange = (e) => {
     setFromValue(e.target.value);
@@ -10,6 +12,20 @@ const Nav = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("submitting");
+
+    // Any AJAX calls/HTTP REQUEST using axios/fetch will return a Promise => response
+    axios
+      .get(
+        ` https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBj3VcWGTjdYsabCaa-fqNFbL2KEAimeOE`
+      )
+      // whenever we get a response back, only then will then() run
+      // we no longer need to use json()
+      .then((response) => {
+        console.log(response.data.items[0].id.videoId);
+        setVideoId(response.data.items[0].id.videoId);
+      })
+      .catch((err) => console.error(err));
   };
   return (
     <div>
@@ -18,6 +34,8 @@ const Nav = () => {
         <input onChange={handleChange} type="text" value={formValue} />
         <button type="submit">Click</button>
       </form>
+
+      {videoId}
     </div>
   );
 };
