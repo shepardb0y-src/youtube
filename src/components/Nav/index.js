d247ed9ebaae8f8a94e8eb0e61e28d7232d3d06d
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-01-30 08:14:09 -0500

    installed bootstrap ansd startd styling login and nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,62 +1,33 @@
 import React from "react";
-import { useState } from "react";
-import axios from "axios";
-
+import "./style.css";
+import Search from "../../pages/Search";
+import Login from "../../pages/Login";
 const Nav = () => {
-  const [formValue, setFromValue] = useState("");
-  const [videoArray, setVideoArray] = useState([]);
-  const baseUrl = "https://youtube.googleapis.com/youtube/v3/search?";
-  const apiKey = "&key=AIzaSyB5LRryc3dUt4OujKIXptSJcX46XTnUlvY";
-  const qparam = "&type=video&part=snippet&maxResults=";
-  const maxResult = "10&q=";
-
-  const handleChange = (e) => {
-    setFromValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submitting");
-
-    axios
-      .get(`${baseUrl}${apiKey}${qparam}${maxResult} ${formValue}`)
-      .then((videoArray) => {
-        console.log(videoArray.data.items);
-        const videoIdArray = videoArray.data.items.map(
-          (v) => "https://www.youtube.com/embed/" + v.id.videoId
-        );
-        setVideoArray(videoIdArray);
-        console.log(videoIdArray);
-      })
-      .catch((err) => console.error(err));
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit} action="">
-        <label htmlFor="Search-bar">Search:</label>
-        <input onChange={handleChange} type="text" value={formValue} />
-        <button type="submit">Click</button>
-      </form>
-
-      {videoArray.map((url) => {
-        console.log(url);
-        let iframe = (
-          <div>
-            <iframe
-              width="560"
-              height="315"
-              src={url}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        );
-        return iframe;
-      })}
-    </div>
+    <ul className="nav">
+      <li className="nav-item">
+        <a
+          className="nav-link active"
+          aria-current="page"
+          href="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1004px-Logo_of_YouTube_%282015-2017%29.svg.png"
+        ></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          <Search />
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Link
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled">
+          <Login />
+        </a>
+      </li>
+    </ul>
   );
 };
 
