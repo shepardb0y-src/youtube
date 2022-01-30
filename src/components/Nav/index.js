72a969b7a9eb9066aa95b9090883c13e734324ae
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-01-30 12:53:36 -0500

    added youtube image to nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,33 @@
 import React from "react";
 import "./style.css";
 import Search from "../../pages/Search";
-import Login from "../../pages/Login";
+
 const Nav = () => {
   return (
-    <ul className="nav">
-      <li className="nav-item">
-        <a
-          className="nav-link active"
-          aria-current="page"
-          href="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1004px-Logo_of_YouTube_%282015-2017%29.svg.png"
-        ></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          <Search />
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled">
-          <Login />
-        </a>
-      </li>
-    </ul>
+    <div id="navbar">
+      <ul className="nav">
+        <li className="nav-item">
+          <img
+            className="youtube-logo"
+            src="https://cdn1.iconfinder.com/data/icons/follow-the-herd/512/youtube_right-512.png"
+            alt=""
+          />
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <Search />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">login</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
