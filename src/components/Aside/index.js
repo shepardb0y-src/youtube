121d39afa42efd3ed82ebb977b262c56502d3967
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-02-07 14:41:20 -0500

    updated links in aside component

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -5,18 +5,28 @@ import { Link } from "react-router-dom";
 const Aside = () => {
   return (
     <div className="aside">
-      <Link to="/">
+      <Link className="home-link" to="/">
         <AsideRow title="Home" />
       </Link>
-      <Link to="/Explore">
+      <Link className="explore-link" to="/Explore">
         <AsideRow title="Explore" />
       </Link>
-
-      <AsideRow title="Subscriptions" />
-      <AsideRow title="Originals" />
-      <AsideRow title="Youtube Music" />
-      <AsideRow title="Library" />
-      <AsideRow title="Downloads" />
+      <Link className="title" to="/Error"></Link>
+      <Link className="title" to="/Error">
+        <AsideRow title="Subscriptions" />
+      </Link>
+      <Link className="title" to="/Error">
+        <AsideRow title="Originals" />
+      </Link>
+      <Link className="title" to="/Error">
+        <AsideRow title="Youtube Music" />
+      </Link>
+      <Link className="title" to="/Error">
+        <AsideRow title="Library" />
+      </Link>
+      <Link className="title" to="/Error">
+        <AsideRow title="Downloads" />
+      </Link>
     </div>
   );
 };
--- src/components/AsideRow/index.js
+++ src/components/AsideRow/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 const AsideRow = ({ title }) => {
   return (
