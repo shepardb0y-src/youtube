03309b9064168d12e12d2dd744ed317c8d3027a7
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-02-07 18:23:01 -0500

    changned footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,15 +5,6 @@ const Footer = () => {
   return (
     <div className="footer">
       <div>Created By Fabio Benoit</div>
-      <div>
-        {" "}
-        <a
-          href="https://github.com/shepardb0y-src/youtube/blob/main/README.md"
-          target="_blank"
-        >
-          Read me
-        </a>
-      </div>
     </div>
   );
 };
