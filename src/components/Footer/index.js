600a7b2f16ffcb2262a0ad4589e0f4f942944e17
Fabio Benoit <60862129+shepardb0y-src@users.noreply.github.com>
2022-02-07 18:24:33 -0500

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,14 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <div>Created By Fabio Benoit</div>
-      <div>
-        {" "}
-        <a
-          href="https://github.com/shepardb0y-src/youtube/blob/main/README.md"
-          target="_blank"
-        >
-          Read me
-        </a>
+      
       </div>
     </div>
   );
